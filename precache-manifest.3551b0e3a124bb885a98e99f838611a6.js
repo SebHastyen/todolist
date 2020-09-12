f14b77cde8fe8a4cab8c646a0166d3201593f348
Sebastien D <seb.hastyen@hotmail.fr>
2020-09-12 15:27:31 +0200

    Updates

---
+++ precache-manifest.3551b0e3a124bb885a98e99f838611a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a2a3dc58eccd7381332e1f8e50c7e2",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10165380a82535bb12a1",
+    "url": "/todolist/static/js/main.da42b8c0.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
